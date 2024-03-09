777a21605ff479d196169317ac4f5fa932df00b1
Amirmahdi Nourkazemi <nourkazemi80@gmail.com>
2024-03-09 23:45:26 +0330

    feat:add animation for seven section center text

--- lib/constant/color.dart
+++ lib/constant/color.dart
@@ -3,4 +3,5 @@ import 'package:flutter/material.dart';
 class AppColors {
   static const Color scaffoldColor = Color(0xFF1A1A1A);
   static const Color secondaryColor = Color(0xFFC9FA49);
+    static const Color darkColor = Color(0xff2A2A2A);
 }
--- lib/screen/sections/seventh_section.dart
+++ lib/screen/sections/seventh_section.dart
@@ -1,5 +1,9 @@
 import 'package:crypto_ui_web/constant/color.dart';
+import 'package:crypto_ui_web/screen/widget/text_reveal.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../bloc/screen_offset.dart';
 
 class SeventhSection extends StatefulWidget {
   const SeventhSection({super.key});
@@ -8,25 +12,182 @@ class SeventhSection extends StatefulWidget {
   State<SeventhSection> createState() => _SeventhSectionState();
 }
 
-class _SeventhSectionState extends State<SeventhSection> {
+class _SeventhSectionState extends State<SeventhSection>
+    with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+  late Animation<double> imageRevealAnimation;
+  late Animation<double> textRevealAnimation;
+  late Animation<double> subTextOpacityAnimation;
+  late Animation<double> subImageRevealAnimation;
+  late Animation<Offset> offsetImage;
+  late Animation<Offset> transform;
+
+  @override
+  void initState() {
+    controller = AnimationController(
+      vsync: this,
+      duration: const Duration(
+        milliseconds: 1700,
+      ),
+      reverseDuration: const Duration(
+        milliseconds: 375,
+      ),
+    );
+
+    imageRevealAnimation = Tween<double>(begin: 500.0, end: 0.0).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.0, 0.40, curve: Curves.easeOut)));
+
+    textRevealAnimation = Tween<double>(begin: 70.0, end: 0.0).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.30, 0.60, curve: Curves.easeOut)));
+
+    subTextOpacityAnimation = Tween<double>(begin: 0.0, end: 1.0).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.50, 0.80, curve: Curves.easeOut)));
+    offsetImage =
+        Tween<Offset>(begin: const Offset(-1, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+    transform =
+        Tween<Offset>(begin: const Offset(10, 0), end: const Offset(0, 0))
+            .animate(CurvedAnimation(parent: controller, curve: Curves.ease));
+
+    subImageRevealAnimation = Tween<double>(begin: 0.0, end: 90.0).animate(
+        CurvedAnimation(
+            parent: controller,
+            curve: const Interval(0.70, 1.0, curve: Curves.easeOut)));
+
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    controller.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: MediaQuery.of(context).size.width,
-      height: 350,
-      decoration: BoxDecoration(
-        color: AppColors.secondaryColor,
-      ),
-      child: Center(
-        child: Text(
-          'Start Your Crypto Odyssey With Us',
-          style: TextStyle(
-              fontFamily: 'CH',
-              color: Color(0xff2A2A2A),
-              fontSize: 25,
-              fontWeight: FontWeight.bold),
-        ),
-      ),
+    return BlocBuilder<DisplayOffset, ScrollOffset>(
+      buildWhen: (previous, current) {
+        if ((current.scrollOffsetValue > 3400 &&
+                current.scrollOffsetValue < 4000) ||
+            controller.isAnimating) {
+          return true;
+        } else {
+          return false;
+        }
+      },
+      builder: (context, state) {
+        if (state.scrollOffsetValue >= 3400 &&
+            state.scrollOffsetValue <= 3900) {
+          controller.forward();
+        } else {
+          controller.reverse();
+        }
+        return AnimatedBuilder(
+          animation: textRevealAnimation,
+          builder: (context, child) {
+            return Container(
+              width: MediaQuery.of(context).size.width,
+              height: 350,
+              decoration: const BoxDecoration(
+                color: AppColors.secondaryColor,
+              ),
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    TextReveal(
+                      controller: controller,
+                      maxHeight: 80,
+                      child: const Text(
+                        'Start Your Crypto Odyssey With Us',
+                        style: TextStyle(
+                          fontFamily: 'CH',
+                          color: Color(0xff2A2A2A),
+                          fontSize: 25,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    TextReveal(
+                      maxHeight: 100,
+                      controller: controller,
+                      child: const Text(
+                        'Lorem ipsum dolor sit amet consectetur. Duis morbi scelerisque \nlectus sodales rhoncus.',
+                        style: TextStyle(
+                          fontFamily: 'CH',
+                          color: Color(0xff2A2A2A),
+                          fontSize: 15,
+                          fontWeight: FontWeight.w400,
+                        ),
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        TextReveal(
+                            maxHeight: 80,
+                            controller: controller,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                fixedSize: const Size(150, 50),
+                                backgroundColor: AppColors.darkColor,
+                              ),
+                              child: const Text(
+                                'Get Started',
+                                style: TextStyle(
+                                  fontFamily: 'CH',
+                                  fontSize: 13,
+                                  color: AppColors.secondaryColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            )),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        TextReveal(
+                            maxHeight: 80,
+                            controller: controller,
+                            child: ElevatedButton(
+                              onPressed: () {},
+                              style: ElevatedButton.styleFrom(
+                                  fixedSize: const Size(150, 50),
+                                  backgroundColor: AppColors.secondaryColor,
+                                  side: const BorderSide(
+                                      color: AppColors.darkColor)),
+                              child: const Text(
+                                'Learn more',
+                                style: TextStyle(
+                                  fontFamily: 'CH',
+                                  fontSize: 13,
+                                  color: AppColors.darkColor,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ))
+                      ],
+                    )
+                  ],
+                ),
+              ),
+            );
+          },
+        );
+      },
     );
   }
 }
